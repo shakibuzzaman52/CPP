8c4c0ae50e6757f25731fcb2492b3ef533e8fed8
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-06-27 14:38:20 +0600

    Assignment operator

---
+++ assignment_oper.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a = 3;
+    int b = 2;
+
+
+    a += b;
+    cout<<a<<endl;
+    
+    a -= b;
+    cout<<a<<endl;
+
+    a *= b;
+    cout<<a<<endl;
+
+    a /= b;
+    cout<<a<<endl;
+
+
+    getch();
+}
