d0a8b04af6da69b9dc9e1ad0940773b2df0bd253
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-06-29 22:38:55 +0600

    Bangladesh 10 times with while loops

---
+++ while_loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int i = 1;
+    while(i <= 10)
+    {
+        cout<<"Bangladesh : "<<i<<endl;
+        i++;  
+    }
+
+
+    getch();
+}
