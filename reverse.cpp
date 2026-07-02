3710562fb53ff7996bc2dad2c0f65d2895e958ab
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-07-02 18:47:02 +0600

    reverse string

--- reverse.cpp
+++ reverse.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int number, rem, temp;
-    int reverse = 0;
+    int sum = 0;
     cout<<"Enter a digit :";
     cin>>number;
 
@@ -14,11 +14,11 @@ int main()
     while(temp != 0)
     {
         rem = temp % 10;
-        reverse = (reverse) * 10 + rem;
+        sum = (sum) * 10 + rem;
         temp = temp / 10;
 
     }
-    cout<<"Reverse is :"<<reverse;
+    cout<<"Reverse is :"<<sum;
 
 
     getch();
