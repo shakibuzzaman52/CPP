0f787f8b1ff3989d997ade163950fbb13f63bb3a
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-07-02 20:18:04 +0600

    sum_of_even_num.cpp

---
+++ sum_of_even_num.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter last even number : ";
+    cin>>n;
+
+    int sum = 0;
+    for(int i = 2; i <= n; i += 2)
+    {
+        sum = sum + i;
+    }
+    cout<<"Sum of even number is :"<<sum;
+
+
+    getch();
+}
