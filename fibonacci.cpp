03a4792bf260a0f71ad7df4050e51ab2ab9bfd5c
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-07-03 23:33:08 +0600

    0 1 1 2 3 4 7..

---
+++ fibonacci.cpp
@@ -0,0 +1,30 @@
+//0 1 1 2 3 4 7..
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int first = 0, second = 1, count = 0,n, fibo;
+    cout<<"Enter a number :";
+    cin>>n;
+
+    while(count < n)
+    {
+        if(count <= 1)
+        {
+            fibo = count;
+        }
+        else
+        {
+            fibo = first + second;
+            first = second;
+            second = fibo;
+        }
+        cout<<fibo<<" ";
+        count++;
+        
+    }
+
+    
+    getch();
+}
