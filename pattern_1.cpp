de3412de68a133ed7074560084e129911320a0c8
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-07-05 12:12:55 +0600

    1 12 123

---
+++ pattern_1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, row, col;
+    cout<<"Enter a number :";
+    cin>>n;
+
+    for(row = 1; row <= n; row++)
+    {
+        for(col = 1; col <= row; col++)
+        {
+            cout<<col;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
