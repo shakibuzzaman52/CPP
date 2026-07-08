d09277908ae938ebd9d6a60bda5424cf12455f25
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-07-08 13:30:59 +0600

    Combined 2 types of piradio pattern

---
+++ pattern_9.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, row, col;
+    cout<<"Enter a number :";
+    cin>>n;
+
+    for(row = 1; row <= n; row++)
+    {
+        for(col = 1; col <= n - row; col++)
+        {
+            cout<<" ";
+        }
+        for(col = 1; col <= 2*row - 1; col ++)
+        {
+            cout<<col;
+        }
+        cout<<endl;
+    }
+
+    for(row = n - 1; row >= 1; row--)
+    {
+        for(col = 1; col <= n - row; col++)
+        {
+            cout<<" ";
+        }
+        for(col = 1; col <= 2*row - 1; col ++)
+        {
+            cout<<col;
+        }
+        cout<<endl;
+    }
+
+    
+    return 0;
+}
