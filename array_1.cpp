1abf527d75044bdeed220fce0c4c083e1449f036
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-07-09 12:22:58 +0600

    Array basic

---
+++ array_1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int nums[5] = {23, 34, 54, 67, 55};
+
+    for(int i = 0; i <= 4; i++)
+    {
+        cout<<nums[i]<<" ";
+    }
+
+    return 0;
+}
