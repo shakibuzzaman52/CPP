2740515f4fb6173b375da12eeb673565cb5b9142
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-07-11 13:33:52 +0600

    2d array input

--- 2d_array_input.cpp
+++ 2d_array_input.cpp
@@ -4,21 +4,23 @@ int main()
 {
     int row, col, A[2][3];
 
-    cout<<"Enter the elements for the matrix :";
+    cout<<"Enter the elements for the matrix :"<<endl;
     for(row = 0; row < 2; row++)
     {
         for(col = 0; col < 3; col++)
         {
+            cout<<"A ["<<row<<"]["<<col<<"] = ";
             cin>>A[row][col];
         }
     }
 
-    cout<<"The matrix is : ";
+    cout<<"The matrix is : "<<endl;
     for(row = 0; row < 2; row++)
     {
         for(col = 0; col < 3; col++)
         {
-            cout<<A[row][col]<<" ";
+            cout<<"A ["<<row<<"]["<<col<<"] = ";
+            cout<<A[row][col]<<" "<<endl;
         }
     }
     
