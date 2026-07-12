e0226c66dd7688548a27dfbaca10c5fa73cdd271
Shakib Uz Zaman <shakibuzzaman52@gmail.com>
2026-07-12 23:08:31 +0600

    1 + 2 + 3 + 4 ...... + n

--- series_1.cpp
+++ series_1.cpp
@@ -1,19 +1,19 @@
 // 1 + 2 + 3 + 4 ...... + n
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter last number :";
-    cin>>n;
+    cout << "Enter last number :";
+    cin >> n;
     int sum = 0;
 
-    for(int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
         sum = sum + i;
     }
-    cout<<"Sum is : "<<sum;
+    cout << "Sum is : " << sum;
 
     getch();
 }
