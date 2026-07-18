a213770419b48bcd72519984ddc2500a2ad2df50
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-07-19 00:10:53 +0600

    Function return

---
+++ function2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int addition(int, int);
+
+int main()
+{
+    cout<<addition(10, 20);
+    
+    return 0;
+}
+int addition(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
