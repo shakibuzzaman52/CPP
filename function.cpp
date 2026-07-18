19b6d85d1959a463588a31410bd1a486ecbe5890
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-07-18 22:21:59 +0600

    Basic of function

---
+++ function.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void addition(int a, int b)
+{
+    int sum = a + b;
+    cout << "sum = " << sum << endl;
+}
+
+int main()
+{
+    addition(10, 20);
+    addition(30, 40);
+
+    return 0;
+}
