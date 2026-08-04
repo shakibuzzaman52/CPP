9f86a495b23fb0c5ea0348ed4b4d73f2d626b0f9
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-04 20:35:44 +0600

    array input output

---
+++ array_input_output.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num[5];
+    for(int i = 0; i < 5; i++)
+    {
+        cin>>num[i];
+    }
+    for(int i = 0; i < 5; i++)
+    {
+        cout<<num[i];
+    }
+    return 0;
+}
