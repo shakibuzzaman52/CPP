a4f1c5d2e6f928d2d9d020e08e278757e0c2d498
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-07 11:27:19 +0600

    function class 1

---
+++ function_class1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void addition(int a, int b)
+{
+    int sum = a + b;
+    cout<<sum<<endl;;
+}
+int main()
+{
+    addition(10, 20);
+    addition(20, 30);
+    return 0;
+}
