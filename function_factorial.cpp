c81feb304ec1df9096837573f210a5825c334316
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-09 01:07:48 +0600

    function with factorial

--- function_factorial.cpp
+++ function_factorial.cpp
@@ -1,17 +1,42 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int factorial(int n)
+// {
+//     int fact = 1;
+//     for(int i = n; i >= 1; i--)
+//     {
+//         fact = fact * i;
+//     }
+
+//     return fact;
+// }
+// int main()
+// {
+//     cout<<factorial(5);
+//     return 0;
+// }
+
+
+// Different way--------------
+
 #include <bits/stdc++.h>
 using namespace std;
-int factorial(int n)
-{
-    int fact = 1;
-    for(int i = n; i >= 1; i--)
-    {
-        fact = fact * i;
-    }
 
-    return fact;
+int fact(int n)
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
 }
+
 int main()
 {
-    cout<<factorial(5);
+    int factorial = fact(4);
+    cout<<factorial;
     return 0;
 }
