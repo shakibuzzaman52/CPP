6f08cde4f0543a127286151956219c1671f87145
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-08 20:24:51 +0600

    nCr with function

--- function_nCr_bionomical_coeficient.cpp
+++ function_nCr_bionomical_coeficient.cpp
@@ -13,14 +13,48 @@ int factOfNumber(int number)
     return factorial;
 }
 
+int nCr(int n, int r)
+{
+    int fact_n = factOfNumber(n);
+    int fact_r = factOfNumber(r);
+    int fact_nmr = factOfNumber(n-r);
+
+    int nCr = fact_n / (fact_r * fact_nmr);
+
+    return nCr;
+}
+
 int main()
 {
-    int n = 8;
-    int r = 2;
-
-    int nCr = factOfNumber(n) / (factOfNumber(r) * factOfNumber(n - r));
-
-    cout << nCr;
-
+    cout<<nCr(8, 2);
     return 0;
 }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int factOfNumber(int number)
+// {
+//     int factorial = 1;
+
+//     for(int i = 1; i <= number; i++)
+//     {
+//         factorial = factorial * i;
+//     }
+
+//     return factorial;
+// }
+
+// int main()
+// {
+//     int n = 8;
+//     int r = 2;
+
+//     int nCr = factOfNumber(n) / (factOfNumber(r) * factOfNumber(n - r));
+
+//     cout << nCr;
+
+//     return 0;
+// }
