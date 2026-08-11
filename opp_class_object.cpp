e7cafc95946b117a4c11271730bbc437755448d0
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-11 15:20:18 +0600

    Opp Class & Object

---
+++ opp_class_object.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class student
+{
+    public:
+    int id;
+    double gpa;
+};
+
+int main()
+{
+    student shakib;
+    shakib.id = 3412;
+    shakib.gpa = 5.00;
+    cout<<shakib.id<<endl<<shakib.gpa<<endl;
+
+    student nishat;
+    nishat.id = 4532;
+    nishat.gpa = 5.00;
+    cout<<nishat.id<<endl<<nishat.gpa;
+
+    return 0;
+}
