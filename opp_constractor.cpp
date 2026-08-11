0ad85797a525b8604d166d59e5681a1670efffd4
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-11 16:23:33 +0600

    Opp Constractor

---
+++ opp_constractor.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ class student  // Class
+ {
+    public:
+        int id;
+        double gpa;
+
+    student(int x, double y)  // Constractor
+    {
+        id = x;
+        gpa = y;
+    }
+
+    void display()
+    {
+        cout<<"Id = "<< id <<endl << "Gpa = "<< gpa<<endl;
+    }
+ };
+int main()
+{
+    student shakib(3412, 5.00); // Object with constractor value
+    shakib.display();
+
+    student nishat(5456, 5.00);
+    nishat.display();
+
+    return 0;
+}
