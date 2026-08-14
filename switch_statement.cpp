a77f4c5d7a0c0fabf1cbe0eddc2fb85e9e63d18c
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-14 20:24:15 +0600

    switch statement

---
+++ switch_statement.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int day;
+    cin>>day;
+    switch(day)
+    {
+        case 1 : cout<< "Mondey";
+           break;
+        case 2 : cout<< " Tuesday";
+           break;
+        case 3 : cout<< "Wednesday";
+           break;
+        case 4 : cout<< "Thusday";
+           break;
+        case 5 : cout<< "Friday";
+           break;
+        case 6 : cout<< "Satarday";
+           break;
+        case 7 : cout<< "Sunday";
+           break;
+
+    }
+    return 0;
+}
