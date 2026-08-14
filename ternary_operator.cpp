74c8f33fd2ae27ebfd0df2f6ddb3c24ac6dccb16
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-14 20:07:31 +0600

    ternary operator

---
+++ ternary_operator.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age;
+    cin>>age;
+    bool isAdult;
+    isAdult = (age >= 18) ? true : false;
+    cout<<isAdult;
+    return 0;
+}
