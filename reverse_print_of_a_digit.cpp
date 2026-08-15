040c24fc34c446dee667f9a83fd71fae67886b08
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-15 19:41:51 +0600

    Reverse Print of a digit

---
+++ reverse_print_of_a_digit.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n > 0)
+    {
+        int lastDig = n % 10;
+        cout<<lastDig;
+        n = n / 10;
+    }
+
+    return 0;
+}
