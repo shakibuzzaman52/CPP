1c64d4053dc842fcb543f6422f440bb3e16012cc
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-16 23:45:00 +0600

    Parameters and arguments

--- parameters_and_arguments.cpp
+++ parameters_and_arguments.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-int sum(int a, int b)  // Parameters
+int sum(int a, int b)  //a , b are Parameters
 {
     int sum = a + b;
     return sum;
 }
 int main()
 {
-    cout<<sum(10, 20); // Arguments
+    cout<<sum(10, 20); // 10, 20 are Arguments
 
     return 0;
 }
