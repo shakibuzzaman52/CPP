d9086440f6995a374c60349d3d65126236b19e63
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-16 23:26:27 +0600

    Basic syntax of void function

---
+++ func.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+void sayHello()
+{
+    cout<<"Hello"<<" ";
+}
+void assistant()
+{
+    sayHello();
+    cout<<"Assistant";
+}
+int main()
+{
+    assistant();
+    return 0;
+}
