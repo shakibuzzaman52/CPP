645bb2223e53d476abad205a751e447cda4da5a8
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-17 01:28:43 +0600

    2 to n all prime number print

---
+++ 2_to_n_prime_number.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isPrime(int n)
+{
+    if(n == 1)
+    {
+        return false;
+    }
+    for(int i = 2; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void allPrimes(int n)
+{
+    for(int i = 2; i <= n; i++)
+    {
+        if(isPrime(i))
+        {
+            cout<<i<<" ";
+        }
+    }
+}
+int main()
+{
+    allPrimes(13);
+    return 0;
+}
