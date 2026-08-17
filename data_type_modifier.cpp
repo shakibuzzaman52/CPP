56ed662c422fbf1453bc99007c65e6afae61cdc8
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-18 01:06:38 +0600

    Data Type Modifier

---
+++ data_type_modifier.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<sizeof(int)<<endl;  // Range 2^31
+    cout<<sizeof(long int)<<endl;
+    cout<<sizeof(short int)<<endl<<endl;
+
+    cout<<sizeof(unsigned int)<<endl<<endl;  // Range 2^32
+
+    cout<<sizeof(long long int)<<endl;
+    cout<<sizeof(long long)<<endl<<endl;
+
+    cout<<sizeof(double)<<endl;
+    cout<<sizeof(long double)<<endl<<endl;
+
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(string)<<endl;
+    return 0;
+}
