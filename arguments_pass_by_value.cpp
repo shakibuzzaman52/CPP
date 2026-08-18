b751579603fabe432c985cb5f44a3541e1c065a9
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-18 15:09:50 +0600

    Arguments Pass by Value

---
+++ arguments_pass_by_value.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void number(int n)
+{
+    n = 20;
+    cout<<n << endl;  // 20
+}
+int main()
+{
+    int n = 10;
+    number(n);
+
+    cout<<n << endl;  // 10
+    return 0;
+}
