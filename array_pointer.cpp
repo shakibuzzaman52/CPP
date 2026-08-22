a31dbb161c1732e1ebe209987001dffed652de56
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-22 17:14:58 +0600

    Array Pointer

---
+++ array_pointer.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {1, 2, 4, 5};
+
+    cout<< arr << endl; // pointer 0x61ff00 location
+    cout<< *arr; // Deference
+    return 0;
+}
