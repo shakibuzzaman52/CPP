c25f42449e31ffd84ba635ee09d3c808e2b92824
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-08-23 15:04:17 +0600

    Pointer Increment and Decrement

--- pointer_arithmetic.cpp
+++ pointer_arithmetic.cpp
@@ -7,11 +7,13 @@ int main()
     int * ptr = &a;
     cout<<ptr  << endl;
 
-    ptr++; // 4 bytes add for int
-    cout<<ptr << endl;
+    cout<<ptr++ << endl;  // 4 bytes add for int
 
-    ptr--; // 4 bytes add for int
-    cout<<ptr << endl;
+    cout<<ptr-- << endl; // 4 bytes decrease for int
+
+    cout<<ptr + 3 << endl;  // 3*4 bytes add for int
+
+    cout<<ptr - 3<< endl; // 3*4 bytes decrease for int
 
 
     return 0;
