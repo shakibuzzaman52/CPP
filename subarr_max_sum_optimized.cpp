0257173678ae35f1988c50cb474aa3fdf649d3a2
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-09-11 13:01:27 +0600

    Max Sub Array Sum Optimized

---
+++ subarr_max_sum_optimized.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+void subArrSum(int * arr, int n)
+{
+    int maxSum = INT_MIN;
+    for(int i= 0; i < n; i++)
+    {
+        int sum = 0;
+        for(int j = i; j < n; j++)
+        {
+            sum = sum + arr[j];
+        }
+        cout<<sum;
+        maxSum = max(maxSum, sum);
+        cout<<endl;
+    }
+    cout<<"Max Sum = "<< maxSum;
+}
+int main()
+{
+    int arr[6] = {2, -3, 6, -5, 4, 2};
+    subArrSum(arr, 6);
+
+    return 0;
+}
