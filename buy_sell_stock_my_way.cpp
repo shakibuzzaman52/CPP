ab501a86fe0c55e49618ab9a38588d313dd06875
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-09-11 18:02:19 +0600

    Buy Sell And Stock and Max Profit

--- buy_sell_stock_my_way.cpp
+++ buy_sell_stock_my_way.cpp
@@ -3,6 +3,7 @@ using namespace std;
 void maxProfit(int * arr, int n)
 {
     int maxProfit = INT_MIN;
+    int buy_day, sell_day;
     for(int buy = 0; buy < n - 1; buy++)
     {
         for(int sell = buy + 1; sell < n; sell++)
@@ -12,12 +13,16 @@ void maxProfit(int * arr, int n)
             if(profit > maxProfit)
             {
                 maxProfit = profit;
+                buy_day = buy;
+                sell_day = sell;
             }
         }
         
         cout<< endl;
     }
-    cout<<"Max profit = "<<maxProfit;
+    cout<<"Max profit = "<<maxProfit <<endl;
+    cout << "Buy at = " << arr[buy_day] << endl;
+    cout << "Sell at = " << arr[sell_day] << endl;
 }
 int main()
 {
