08905cc956352cddb09458339ef1134cd8c1f480
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-09-13 15:49:36 +0600

    Sort Practice

---
+++ bubble-sort.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int * arr, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout<<arr[i];
+    }
+}
+void bubbleSort(int * arr, int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    print(arr, n);
+}
+int main()
+{
+    int arr[] = {5, 4, 3, 1, 2};
+    int n = sizeof(arr) / sizeof(int);
+    bubbleSort(arr, n);
+    return 0;
+}
---
+++ selecction-sort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int * arr, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout<<arr[i];
+    }
+}
+void selectionSort(int * arr, int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        int minIdx = i;
+        for(int j = i + 1; j < n; j++)
+        {
+            if(arr[j] < arr[minIdx])
+            {
+                minIdx = j;
+            }
+        }
+        swap(arr[i], arr[minIdx]);
+    }
+    print(arr, n);
+}
+int main()
+{
+    int arr[] = {5, 4, 3, 1, 2};
+    int n = sizeof(arr) / sizeof(int);
+    selectionSort(arr, n);
+    return 0;
+}
