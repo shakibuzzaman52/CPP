464d011854626bf1e674d01a82b2de990f60fe2b
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-09-14 19:54:15 +0600

    In Build Sort

---
+++ in_build_sort_custom_range.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int * arr, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout<<arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[] = {6, 4, 5, 1, 7, 2, 8, 0};
+    int n = sizeof(arr) / sizeof(int);
+
+    sort(arr + 2, arr + 6);  // 5 to 8 num sort
+    print(arr, n);
+    return 0;
+}
