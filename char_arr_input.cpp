f359e0b26c0bf9273451c02c9560794d62db1637
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-09-19 22:32:18 +0600

    Word and Sentence Input

---
+++ char_arr_input.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // char word[10];
+    // cin>>word;  // Ignore white space
+    // cout<< "Your word char = " << word << endl;
+    // cout<< strlen(word);
+
+
+    char sentence[30];
+    cin.getline(sentence, 30);
+    cout<< "Your Sentence arr = " << sentence << endl;
+    cout<< strlen(sentence);
+    
+
+    // char sentence_terminate[30];
+    // cin.getline(sentence_terminate, 30, '*');
+    // cout<< "Your sentence terminate at the point of * = " << sentence_terminate << endl;
+    // cout<< strlen(sentence_terminate);
+}
