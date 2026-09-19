377668bf7550f3e5804147b3526982c52c4f1ad5
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-09-19 22:16:08 +0600

    Char Arr Create and Input

---
+++ char_arr_create_output.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char work1[] = "code";
+
+    char work2[5] = "code";
+
+    char work3[5] = { 'c', 'o', 'd', 'e', '\0'};
+
+    cout<< work1 << endl << work2 << endl << work3 << endl;
+
+    cout << strlen(work3);
+    return 0;
+}
