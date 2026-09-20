c59d29155b91537f34ab8e8b95631f3de7b2aa6d
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-09-20 18:07:21 +0600

    Char arr copy

---
+++ char_arr_copy.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char str1[10];
+    strcpy(str1, "Shakib");
+
+    char str2[10];
+
+    strcpy(str2, str1);
+    
+    cout<< str2;
+    return 0;
+}
