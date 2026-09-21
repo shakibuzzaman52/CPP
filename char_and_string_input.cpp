7ca77c1f84946258c083ef58439bb712dbd2b648
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-09-21 22:19:57 +0600

    Char Sentence And String Sentence Input

---
+++ char_and_string_input.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char arr[100];
+    cin.getline(arr, 100);  // Char Sentence Input
+
+    cout<< arr << endl;
+
+
+    string str;
+    getline(cin, str);  // String Sentence Input
+
+    cout<< str << endl;
+    return 0;
+}
