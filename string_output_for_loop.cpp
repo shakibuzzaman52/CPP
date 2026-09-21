f5fcc471ac768d1208007a8da9f80ce3b028d341
Shakib Uz Zaman <207609386+shakibuzzaman52@users.noreply.github.com>
2026-09-21 22:30:42 +0600

    String Output Using For Loop

---
+++ string_output_for_loop.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str = "Shakib Uz Zaman";
+    for(int i = 0; i < str.length(); i++)
+    {
+        cout<< str[i] << " ";
+    }
+    return 0;
+}
